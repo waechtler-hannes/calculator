4db1d03392a00544c990f69e18a11c5d5f9e146b
Hannes <142382297+waechtler-hannes@users.noreply.github.com>
2024-11-07 12:54:56 +0100

    Remove the complexity of the event-listener
    Moved all functionality from the event-listener to seperate functions
    for better readability and easier use.
    Added new display to separate the result from the calculation.

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const display = document.querySelector(".display");
+const calcDisplay = document.querySelector(".calc-display");
+const resultDisplay = document.querySelector(".result-display");
 const buttonContainer = document.querySelector(".button-container");
-buttonContainer.addEventListener("click", (event) => {
-    if (event.target.type === "submit") {
-        if (event.target.textContent === "C") {
-            display.textContent = "";
-        } else if (event.target.textContent.match(/[0-9]/)) {
-            display.textContent = display.textContent + event.target.textContent;
-        } else if (event.target.textContent.match(/[÷ || × || − || +]/)) {
-            if (!display.textContent.match(/[÷ || × || − || +]/)) {
-                display.textContent = display.textContent + " " + event.target.textContent + " ";
-            }
-        } else if (event.target.textContent.match(/[=]/)) {
-            const array = display.textContent.split(" ");
-            display.textContent = operate(parseFloat(array[0]), parseFloat(array[2]), array[1]);
+
+buttonContainer.addEventListener("click", (e) => {
+    if (isButton(e)) {
+        if (isClear(e)) {
+            clearDisplay();
+        } else if (isNumber(e) || isOperator(e)) {
+            updateDisplay(e);
+        } else if (isEqualSign(e)) {
+            showResult();
         }
     }
 })
@@ -44,4 +41,44 @@ function operate(a, b, operator) {
         case "÷":
             return divide(a, b);
     }
+}
+
+function clearDisplay() {
+    calcDisplay.textContent = "";
+    resultDisplay.textContent = "";
+}
+
+function showResult() {
+    const array = calcDisplay.textContent.split(" ");
+    resultDisplay.textContent = operate(parseFloat(array[0]), parseFloat(array[2]), array[1]);
+}
+
+function updateDisplay(e) {
+    if (isNumber(e)) {
+        calcDisplay.textContent += e.target.textContent;
+    } else if (isOperator(e)) {
+        calcDisplay.textContent += " " + e.target.textContent + " ";
+    }
+}
+
+function isButton(e) {
+    return e.target.type === "submit";
+}
+
+function isClear(e) {
+    return e.target.textContent === "C";
+}
+
+function isNumber(e) {
+    return e.target.textContent.match(/[0-9]/)
+}
+
+function isOperator(e) {
+    return !calcDisplay.textContent.match(/[÷ || × || − || +]/)
+        && calcDisplay.textContent.match(/[0-9]/)
+        && e.target.textContent.match(/[÷ || × || − || +]/);
+}
+
+function isEqualSign(e) {
+    return e.target.textContent.match(/[=]/);
 }
